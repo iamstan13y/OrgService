1f2e2f0c2ff715d7106c3d7f4511a5e850baee8a
iamstan13y <iamstan13y@gmail.com>
2021-07-12 14:07:35 +0200

    added a couple of sh_t

---
+++ OrgAPI/Controllers/DepartmentsController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OrgDAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrgAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentsController : Controller
+    {
+        OrganizationDbContext dbContext;
+        public DepartmentsController(OrganizationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IEnumerable<Department> Get()
+        {
+            var Depts = dbContext.Departments.ToList();
+            return Depts;
+        }
+
+        [HttpGet("{id}")]
+        public Department Get(int id)
+        {
+            var Dept = dbContext.Departments.Where(x => x.Did == id).FirstOrDefault();
+            return Dept;
+        }
+
+        [HttpDelete("{id}")]
+        public string Delete(int id)
+        {
+            var Dept = dbContext.Departments.Where(x => x.Did == id).FirstOrDefault();
+            dbContext.Remove(Dept);
+            dbContext.SaveChanges();
+            return "Record deleted successfully!";
+        }
+
+        [HttpPost]
+        public Department Post(Department D)
+        {
+            dbContext.Add(D);
+            dbContext.SaveChanges();
+            return D;
+        }
+
+        [HttpPut]
+        public string Put(Department D)
+        {
+            dbContext.Update(D);
+            dbContext.SaveChanges();
+            return "Updated record successfully!";
+        }
+    }
+}
