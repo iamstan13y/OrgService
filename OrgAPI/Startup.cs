d03664b5b50bfcd1c7156b84f6ef407d76e339b2
iamstan13y <iamstan13y@gmail.com>
2021-07-20 15:31:51 +0200

    added OpenAPI.

--- OrgAPI/Startup.cs
+++ OrgAPI/Startup.cs
@@ -23,8 +23,9 @@ namespace OrgAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc(options => options.EnableEndpointRouting = false);
             services.AddDbContext<OrganizationDbContext>();
+            services.AddSwaggerDocument();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
