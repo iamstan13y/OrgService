aa747c3ba963c483b642ac8e1ae601160a04eca7
iamstan13y <iamstan13y@gmail.com>
2021-07-21 23:14:41 +0200

    added Employee model.

---
+++ OrgDAL/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace OrgDAL
+{
+    [Table("Employee")]
+    public class Employee
+    {
+        [Key]
+        public int Eid { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        
+        [ForeignKey("Department")]
+        public int Did { get; set; }
+        public Department Department { get; set; }
+    }
+}
