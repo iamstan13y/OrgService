bea4fe15bab7a313c60ef2868afd4fd7398bef92
iamstan13y <iamstan13y@gmail.com>
2021-07-23 12:05:25 +0200

    Updated database.

---
+++ OrgDAL/Migrations/20210723100425_InitialCreate4.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OrgDAL.Migrations
+{
+    public partial class InitialCreate4 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Id",
+                table: "Employee",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Id",
+                table: "Department",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "Employee");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "Department");
+        }
+    }
+}
