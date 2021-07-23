39cd723bdf58fc87acbeffb50ee7a179b8ccd919
iamstan13y <iamstan13y@gmail.com>
2021-07-23 08:27:05 +0200

    Added register view model.

---
+++ OrgAPI/ViewModel/RegisterViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrgAPI.ViewModel
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
+    }
+}
